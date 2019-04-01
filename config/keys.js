6b3ef3495a06cbbbe3500345bf974b4ee0373761
Kusjay <kelechiuma@gmail.com>
2019-04-01 07:56:14 +0100

    Handled google oauth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://kusjay:goatmeat123@ds155695.mlab.com:55695/storify-dev',
+  googleClientID: '753605726362-rtsofei3hamsmfvkijn69iq06fha5gba.apps.googleusercontent.com',
+  googleClientSecret: 'BxHgA2ZhwiKAfKxD2dtgj99D'
+}
