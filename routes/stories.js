501961566b9af2d4c85c75a87beb0d54093b2233
Kusjay <kelechiuma@gmail.com>
2019-04-03 20:41:18 +0100

    Restricting routes and a few changes

---
+++ routes/stories.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
+
+// Stories Index
+router.get('/', (req, res) => {
+  res.render('stories/index');
+});
+
+// Add Srory Form
+router.get('/add', ensureAuthenticated, (req, res) => {
+  res.render('stories/add');
+});
+
+module.exports = router;
